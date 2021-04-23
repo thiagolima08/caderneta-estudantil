2b8699474a8b4eb831a6b5c59c1aa0c487065323
Danilo Marques <danilomarques20@hotmail.com>
2021-04-23 11:38:46 -0300

    Adicionando logica de autenticação com sessoes. Ajustes nos itens de menu

--- src/main/java/br/edu/ifpb/pweb2/cdi/bean/LoginProfessorBean.java
+++ src/main/java/br/edu/ifpb/pweb2/cdi/bean/LoginProfessorBean.java
@@ -2,14 +2,17 @@ package br.edu.ifpb.pweb2.cdi.bean;
 
 import br.edu.ifpb.pweb2.cdi.controller.ProfessorController;
 import br.edu.ifpb.pweb2.cdi.model.Professor;
+import br.edu.ifpb.pweb2.cdi.util.SessionUtils;
 
+import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpSession;
 import java.io.Serializable;
 
 @Named(value = "loginProfessorBean")
-@ViewScoped
+@SessionScoped
 public class LoginProfessorBean extends GenericBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -28,11 +31,19 @@ public class LoginProfessorBean extends GenericBean implements Serializable {
             boolean verified = professorController.login(professor);
             if (verified) {
                 System.out.println("Logado com sucesso!");
+                HttpSession session = SessionUtils.getSession();
+                session.setAttribute("professor", professor);
                 return "home?faces-redirect=true";
             }
         }
 
 		this.addErrorMessage("Email e/ou senha inválidos");
         return "";
+    }
+
+    public String logout() {
+        HttpSession session = SessionUtils.getSession();
+        session.invalidate();
+        return "index?faces-redirect=true";
     }
 }
--- src/main/java/br/edu/ifpb/pweb2/cdi/view/DataTableView.java
+++ src/main/java/br/edu/ifpb/pweb2/cdi/view/DataTableView.java
@@ -1,50 +1,50 @@
-package br.edu.ifpb.pweb2.cdi.view;
-
-import br.edu.ifpb.pweb2.cdi.controller.AlunoController;
-import br.edu.ifpb.pweb2.cdi.model.Aluno;
-import org.primefaces.PrimeFaces;
-import org.primefaces.event.CellEditEvent;
-import org.primefaces.event.RowEditEvent;
-
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.faces.view.ViewScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
-import java.io.Serializable;
-import java.util.List;
-
-@Named("dtBasicView")
-@ViewScoped
-public class DataTableView implements Serializable {
-
-    private List<Aluno> alunos;
-
-	@Inject
-	private AlunoController alunoController;
-
-    public void init() {
-        this.alunos = alunoController.findAll();
-//        System.out.println(this.alunos);
-    }
-
-
-    public List<Aluno> getAlunos() {
-        return this.alunos;
-    }
-
-
-    public void onRowEdit(RowEditEvent<Aluno> event) {
-        alunoController.update(event.getObject());
-        System.out.println(event.getObject().toString());
-        FacesMessage msg = new FacesMessage("Aluno Editado", String.valueOf(event.getObject().getNome()));
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-
-    public void onRowCancel(RowEditEvent<Aluno> event) {
-        FacesMessage msg = new FacesMessage("Edição Cancelada", String.valueOf(event.getObject().getNome()));
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
-
-}
+//package br.edu.ifpb.pweb2.cdi.view;
+//
+//import br.edu.ifpb.pweb2.cdi.controller.AlunoController;
+//import br.edu.ifpb.pweb2.cdi.model.Aluno;
+//import org.primefaces.PrimeFaces;
+//import org.primefaces.event.CellEditEvent;
+//import org.primefaces.event.RowEditEvent;
+//
+//import javax.annotation.PostConstruct;
+//import javax.faces.application.FacesMessage;
+//import javax.faces.context.FacesContext;
+//import javax.faces.view.ViewScoped;
+//import javax.inject.Inject;
+//import javax.inject.Named;
+//import java.io.Serializable;
+//import java.util.List;
+//
+//@Named("dtBasicView")
+//@ViewScoped
+//public class DataTableView implements Serializable {
+//
+//    private List<Aluno> alunos;
+//
+//	@Inject
+//	private AlunoController alunoController;
+//
+//    public void init() {
+//        this.alunos = alunoController.findAll();
+////        System.out.println(this.alunos);
+//    }
+//
+//
+//    public List<Aluno> getAlunos() {
+//        return this.alunos;
+//    }
+//
+//
+//    public void onRowEdit(RowEditEvent<Aluno> event) {
+//        alunoController.update(event.getObject());
+//        System.out.println(event.getObject().toString());
+//        FacesMessage msg = new FacesMessage("Aluno Editado", String.valueOf(event.getObject().getNome()));
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+//    }
+//
+//    public void onRowCancel(RowEditEvent<Aluno> event) {
+//        FacesMessage msg = new FacesMessage("Edição Cancelada", String.valueOf(event.getObject().getNome()));
+//        FacesContext.getCurrentInstance().addMessage(null, msg);
+//    }
+//
+//}
